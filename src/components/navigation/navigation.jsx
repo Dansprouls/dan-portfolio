5a2921db41660b7363e81c2294edded6dbc05af9
Dansprouls <dspr1015@gmail.com>
2023-11-02 22:51:48 -0400

    responsive scss added to all pages

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,14 +1,29 @@
-import { Link } from "react-router-dom"; //use this to set up navigation
-// import ReorderIcon from "@mui/icons-material/Reorder";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import ReorderIcon from "@mui/icons-material/Reorder";
 
 import "./navigation.scss";
 import PixelPic from "../../assets/pixel-picture.jpeg";
 
 export const NavigationBar = () => {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
   return (
-    <div className="navbar">
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
       <div className="togglebtn">
-        <button></button>
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
       </div>
 
       <div className="nav-links-container">
